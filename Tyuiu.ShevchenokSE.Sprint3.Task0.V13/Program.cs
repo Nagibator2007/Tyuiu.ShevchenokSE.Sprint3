c770bbde230d73102bee5861e099975cfc7d48e3
Nagibator2007 <shevchenokse@outlook.com>
2025-11-28 11:19:22 +0500

    Add project files.

---
+++ Tyuiu.ShevchenokSE.Sprint3.Task0.V13/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.ShevchenokSE.Sprint3.Task0.V13.Lib;
+namespace Tyuiu.ShevchenokSE.Sprint3.Task0.V13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Шевченок С.Е. | СМАРТб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла for                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #13                                                             *");
+            Console.WriteLine("* Выполнил: Шевченок Сергей Евгеньевич | СМАРТб-25-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл for, которая вычисляет сумму ряда     *");
+            Console.WriteLine("* по формуле, при a = 0,5                                                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            double value = 0.5;
+            int startValue = 1;
+            int stopValue = 20;
+            Console.WriteLine("Переменная X = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Сумма ряда = " + ds.GetSumSeries(value, startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
