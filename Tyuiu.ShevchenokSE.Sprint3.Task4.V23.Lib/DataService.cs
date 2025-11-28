68f3e649a76b57c1ac3a37cf1ed57b5c4b4f10f3
Nagibator2007 <shevchenokse@outlook.com>
2025-11-28 19:03:46 +0500

    fix task4

--- Tyuiu.ShevchenokSE.Sprint3.Task4.V23.Lib/DataService.cs
+++ Tyuiu.ShevchenokSE.Sprint3.Task4.V23.Lib/DataService.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using tyuiu.cources.programming.interfaces.Sprint3;
-
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
 namespace Tyuiu.ShevchenokSE.Sprint3.Task4.V23.Lib
 {
     public class DataService : ISprint3Task4V23
@@ -10,10 +8,11 @@ namespace Tyuiu.ShevchenokSE.Sprint3.Task4.V23.Lib
             double res = 0;
             for (int x = startValue; x <= stopValue; x++)
             {
-                if (x == 0)
-                    continue;
-
-                res += (Math.Cos(x) + 3) / x;
+                if (x == 0) break;
+                else
+                {
+                    res += (Math.Cos(x) / x) + 3;
+                }
             }
             return Math.Round(res, 3);
         }
