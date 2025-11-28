dbfae458e372379886b719b4c82f26b2b916bb94
Nagibator2007 <shevchenokse@outlook.com>
2025-11-28 12:14:12 +0500

    Task 1

---
+++ Tyuiu.ShevchenokSE.Sprint3.Task1.V28/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.ShevchenokSE.Sprint3.Task1.V28.Lib;
+namespace Tyuiu.ShevchenokSE.Sprint3.Task1.V28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Шевченок С.Е. | СМАРТб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла while                                              *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #28                                                             *");
+            Console.WriteLine("* Выполнил: Шевченок Сергей Евгеньевич | СМАРТб-25-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл while, которая вычисляет сумму ряда   *");
+            Console.WriteLine("* по формуле, при a=0,5                                                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            double value = 0.5;
+            int startValue = 1;
+            int stopValue = 18;
+            Console.WriteLine(" переменная х = " + value);
+            Console.WriteLine(" старт шага" + startValue);
+            Console.WriteLine(" конец шага" + stopValue);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Сумма = " + ds.GetSumSeries(value, startValue, stopValue));
+            Console.ReadKey();
+
+        }
+    }
+}
